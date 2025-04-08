a0bd7c80ce319ada9a413166d8c6ab814b683d9a
Tim F.M. Rodgers <38299576+tfmrodge@users.noreply.github.com>
2025-04-08 07:41:05 -0700

    20250408_moreStormpond
    Continue writing stormpond method. Trying to make it compatible but wondering if I am going to have to rewrite everything anyways

--- Stormpond.py
+++ Stormpond.py
@@ -81,9 +81,89 @@ class StormPond(SubsurfaceSinks):
         if flow_time is not None:
             res = flow_time
         else:
-            res = self.flow_time(locsumm,params,['water'],timeseries,ponddims,dx)
+            res = self.flow_time(locsumm,params,['water','subsoil'],timeseries,ponddims,dx)
+        #flow_time set up time-varying parameters. Now we need the rest. Note that this is memory-intensive, so 
+        #may at some point be worth just calculating these values when needed rather than storing them all in RAM
+        for jind, j in enumerate(numc):
+            #Area (A), Volume (V), Density (rho), organic fraction (foc), ionic strength (Ij)
+            #water fraction (fwat), air fraction (fair), temperature (tempj), pH (phj)
+            jind = jind+1 #The compartment number, for the advection term
+            Aj, Vj, Vjind, rhoj, focj, Ij = 'A' + str(j), 'V' + str(j), 'V' + str(jind),'rho' + str(j),'foc' + str(j),'I' + str(j)
+            fwatj, fairj, tempj, pHj = 'fwat' + str(j), 'fair' + str(j),'temp' + str(j), 'pH' + str(j)
+            rhopartj, fpartj, advj = 'rhopart' + str(j),'fpart' + str(j),'adv' + str(j)
+            compartment = j        
+            res.loc[:,Aj] = locsumm.loc[compartment,'Depth']*locsumm.loc[compartment,'Width']
+            #res.loc[:,Vj] = locsumm.loc[compartment,'Depth']*locsumm.loc[compartment,'Width']
             
-        
+            if compartment in ['water']:
+                res.loc[:,Aj] = res.crossarea#Flowing cross-section
+                res.loc[:,Vjind] = res.Vwater
+            elif compartment in ['subsoil']:#Subsoil is channel bed
+                res.loc[mask,Aj] = res.channelarea #surface area of contact with water, so channel area
+                res.loc[:,Vj] = res.loc[:,Vjind] = res.loc[mask,Aj]*locsumm.loc[compartment,'Depth']#Depth represents "active" channel bed
+            
+            
+            if compartment in ['rootbody', 'rootxylem', 'rootcyl']: 
+                mask = res.dm
+                if compartment in ['rootbody']:
+                    res.loc[mask,Vj] = (params.val.VFrootbody+params.val.VFapoplast)*res.Vroot #Main body consists of apoplast and cytoplasm
+                else:
+                    VFrj = 'VF' + str(j)
+                    #Calculated with the volume fraction
+                    res.loc[mask,Vj] = params.loc[VFrj,'val']*res.Vroot
+                    #Area of roots - this is calculating the surface area of the roots as a fraction of the overall root area.
+                    #We can derive this using the lateral surface area of the roots (neglect tips) and the area & volume fractions
+                    # We know that V2/V1 = VF12, and we have V1/A1 = X from the params file. So, A2 = VF12^(-1/3)*A1/V1*V2
+                    if compartment in ['rootxylem']: #xylem surrounds the central cylinder so need to add the fractions and the volumes
+                        res.loc[mask,Aj] = (params.val.VFrootxylem+params.val.VFrootcyl)**(-1/3)*res.Aroot/res.Vroot\
+                        *(params.val.VFrootxylem+params.val.VFrootcyl)*res.Vroot
+                    else:
+                        res.loc[mask,Aj] = params.loc[VFrj,'val']**(-1/3)*res.Aroot/res.Vroot*res.loc[mask,Vj]
+                res.loc[mask,Vjind] = res.loc[mask,Vj] #Needed for the FugModel module
+                res.loc[mask,advj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Q_out']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+            elif compartment in ['water','subsoil']: #water and subsoil
+                #res.loc[mask,advj] = 0;#Exfiltration is dealt with separately as D_exf. May need to add soil removal.
+            else:#Other compartments aren't discretized
+                mask = res.dm==False
+                res.loc[mask,Vj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'V']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+                res.loc[mask,Vjind] = res.loc[mask,Vj] #Needed for FugModel ABC
+                res.loc[mask,Aj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Area']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+                res.loc[mask,advj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Q_out']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+            res.loc[mask,focj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'FrnOC']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #Fraction organic matter
+            res.loc[mask,Ij] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'cond']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0]*1.6E-5 #Ionic strength from conductivity #Plants from Trapp (2000) = 0.5
+            res.loc[mask,fwatj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'FrnWat']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #Fraction water
+            res.loc[mask,fairj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Frnair']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #Fraction air
+            res.loc[mask,fpartj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'FrnPart']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #Fraction particles
+            res.loc[mask,tempj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Temp']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] + 273.15 #Temperature [K]
+            res.loc[mask,pHj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'pH']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #pH
+            res.loc[mask,rhopartj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'PartDensity']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #Particle density
+            res.loc[mask,rhoj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Density']),
+                         index=res.index.levels[0]).reindex(res.index,level=0)[0] #density for every x [M/L³]
+            if compartment == 'air': #Set air density based on temperature
+                res.loc[mask,rhoj] = 0.029 * 101325 / (params.val.R * res.loc[:,tempj])
+            elif compartment in ['topsoil']:
+                res.loc[mask,'Qet'] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'QET']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+                res.loc[mask,'Qtopsoil_subsoil'] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Q_towater']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+                res.loc[mask,'Qsubsoil_topsoil'] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Q_in']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+                #Advective out of system is exfiltration, ET goes to plants.
+                res.loc[mask,advj] = pd.DataFrame(np.array(timeseries.loc[(slice(None),compartment),'Q_exf']),
+                             index=res.index.levels[0]).reindex(res.index,level=0)[0]
+                
         return res
     
     def flow_time(self,locsumm,params,numc,timeseries,ponddims,dx=None):
@@ -164,7 +244,10 @@ class StormPond(SubsurfaceSinks):
                 res.loc[:,'toparea'] = interps['W'](res.x,locsumm.Depth.water)*res.dx
                 res.loc[:,'channelarea'] = interps['CA'](res.x,locsumm.Depth.water)*res.dx
                 res.loc[:,'crossarea'] = interps['XA'](res.x,locsumm.Depth.water)#*res.dx
+                res.loc[:,'Aplants']=params.val.LAI*(interps['W'](res.x,
+                   ast.literal_eval(params.val.dimdict)['overflow'])*res.dx-res.toparea)#assume uniform plant coverage for emergent areas
                 res.loc[:,'Vwater'] = res.crossarea*res.dx
+                #res.loc[:,plantarea]
                 #res.loc[:,'W'] = interps['W'](res.x,locsumm.Depth.water)
                 #res.loc[:,'Vwater'] = locsumm.Depth.Water*
             else:                
@@ -200,40 +283,79 @@ class StormPond(SubsurfaceSinks):
                     x = 0.622*P_rh/(100-P_sat) #kg water/kg air
                     gs = theta*A*(xs-x)/1000#m³/hr (assuming 1000kg/m³)
                     return gs
+                def pond_inf(A,Kinf): #Assume no downstream limit
+                    Qinf = Kinf*A #Assuming just exfiltrating at Kinf
+                    return Qinf
                 #Define function to minimize and find new depth based on new volume
-                def pond_depth(pond_depth):
+                def calc_pond_depth(pond_depth):
                     test_toparea = interps['W'](res.x,pond_depth)*res.dx
                     test_channelarea = interps['CA'](res.x,pond_depth)*res.dx
                     test_XA = interps['XA'](res.x,pond_depth)#*res.dx
-                    nomV = res.crossarea*res.dx
-                    Vevap = pond_evap(test_toparea,timeseries.WindSpeed[t],timeseries.RH[t],
-                                      timeseries.Tair[t],timeseries.Twater[t])
-                    testV = oldV+inflow*dt-outflow*dt
+                    nomV = (test_XA*res.dx).sum()
+                    #Limits to max available volume. 
+                    Vevap = np.min([test_XA*res.dx,dt*pond_evap(test_toparea,timeseries.WindSpeed[t],timeseries.RH[t],
+                                      timeseries.Tair[t],timeseries.Twater[t])],axis=0)#m3/hr*hr
+                    Vinf = np.min([test_XA*res.dx-Vevap,dt*pond_inf(test_channelarea,params.val.Kn)],axis=0)#m3/hr*hr. Assunming saturated conditions
+                    V_transp = np.min([test_XA*res.dx-Vevap-Vinf,dt*res.Aplants*params.val.Emax],axis=0)
+                    testV = max(0,oldV+Vr+Vin-Vout-Vevap.sum()-Vinf.sum())#Limit to zero just in case.
                     minimizer = abs(nomV - testV)
                     return minimizer
-                testdepth = 0.5
-                res.loc[:,'Depth'] = optimize.newton(pond_depth,testdepth,tol=1e-5)
+                testdepth = res.loc[0,'Depth']#Uniform water surface, just take first
+                pond_depth = optimize.newton(calc_pond_depth,testdepth,tol=1e-5)
+                #Then, calculate areas,flows, etc based on this calculated depth
+                res.loc[:,'Depth'] = pond_depth
+                res.loc[:,'toparea']  = interps['W'](res.x,pond_depth)*res.dx
+                res.loc[:,'channelarea'] = interps['CA'](res.x,pond_depth)*res.dx
+                res.loc[:,'crossarea']  = interps['XA'](res.x,pond_depth)
+                res.loc[:,'Aplants']=params.val.LAI*(interps['W'](res.x,
+                   ast.literal_eval(params.val.dimdict)['overflow'])*res.dx-res.toparea)
+                res.loc[:,'Vwater'] = res.crossarea*res.dx
+                res.loc[:,'Qevap'] = np.min([res.crossarea*res.dx/dt,pond_evap(res.toparea,timeseries.WindSpeed[t],timeseries.RH[t],
+                                  timeseries.Tair[t],timeseries.Twater[t])],axis=0)#m3/hr
+                res.loc[:,'Qwaterexf'] = np.min([1/dt*(res.crossarea*res.dx-res.Qevap),pond_inf(res.channelarea,params.val.Kn)],axis=0)#m3/hr
+                res.loc[:,'Qet'] = np.min([1/dt*(res.crossarea*res.dx-res.Qevap-res.Qwaterexf),res.Aplants*params.val.Emax],axis=0)#m³/h
                 #res = self.simple_route(res,inflow,outflow,rainrate,dt,params,ponddims)
             else:
-                print('Need to implement routing')
+                print('Need to implement alternative routing')
+            #Next, lets do some particle settling! Using the equation of Ferguson and Church (2004)
+            #following the implementation shown at https://stormwaterbook.safl.umn.edu/other-resources/appendices/importance-particle-size-distribution-performance-sedimentation
+            def particle_settling(psd,T,R=2.5,C=1.0):
+                #psd should be a 2D array where x is particle size (mm), y  mass%
+                #v = gRd**2/(18*nu+(0.75*C*g*R*d**3)**1/2)
+                #Calculate kinematic viscosity of water
+                # Temperature in °C
+                temps = np.array([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50,
+                                  55, 60, 65, 70, 75, 80, 85, 90, 95, 100])
+                # Kinematic viscosity of water in m²/s (from CRC Handbook or IAPWS data)
+                kin_viscs = np.array([
+                    1.787e-6, 1.519e-6, 1.308e-6, 1.139e-6, 1.004e-6,
+                    0.902e-6, 0.801e-6, 0.729e-6, 0.658e-6, 0.607e-6,
+                    0.553e-6, 0.514e-6, 0.475e-6, 0.445e-6, 0.415e-6,
+                    0.392e-6, 0.368e-6, 0.350e-6, 0.331e-6, 0.312e-6,
+                    0.294e-6])
+                nu = np.interp(T, temps, kin_viscs) #Assume constant T
+                #Then, calculate particle settling velocity for each particle size
+                psd_df = pd.DataFrame(psd)
+                psd_df.loc[:,'vs']=(9.81*R*pds_df.d**2)/(18*nu+(0.75*C*9.81*R*pds_df.d**3)**(1/2)
+                return psd_df
+                
+                
+                
+            psd = np.array(ast.literal_eval(params.val.psd))
             res_t[t] = res.copy(deep=True)
             #Add the 'time' to the resulting dataframe, update some parameters
             res_t[t].loc[:,'time'] = timeseries.loc[t,'time']
             res_t[t].loc[:,'RH'] = timeseries.loc[t,'RH']
             res_t[t].loc[:,'RainRate'] = timeseries.loc[t,'RainRate']
-            for j in numc:
+            for j in numc:#These were here in the BioretentionBlues code, may make more sense elsewhere but leaving for now
                 Tj,pHj = 'T'+str(j),'pH'+str(j)
-                res_t[t].loc[j,'Temp'] = timeseries.loc[t,Tj] #compartment temperature
-                res_t[t].loc[j,'pH'] = timeseries.loc[t,pHj] #Compartment pH
+                res_t[t].loc[:,'temp'+j] = timeseries.loc[t,Tj] #compartment temperature
+                res_t[t].loc[:,'pH'+j] = timeseries.loc[t,pHj] #Compartment pH
+            #We will also put th
+            # 
             #For now, assume that inflow only to the water compartm     
-    
+        #Final dataframe should be a 3-level multiindex of chemical,time,space. This will be time and space
         res_time = pd.concat(res_t)
-        #The structure of this is set up such that V(t) = V(t-1)+sum(Qt). What this means in practice is that the flows
-        #are acting on the previous step's V. For the contaminant transport, we need to have the flows acting on the volumes
-        #in the same time step. To accomplish this, we will shift it up one and add zeros in the first timestep.
-        res_time.loc[:,'V'] = res_time.loc[:,'V'].groupby(level=1).shift(1)
-        #Initial condition was zero volume in all cells. 
-        res_time.loc[(min(res_time.index.levels[0]),slice(None)),'V'] = 0
         #Now we will also calculate advection from the air compartment - side area of air control volume * windspeed*3600 (windspeed in m/s)
         res_time.loc[(slice(None),'air'),'Q_out'] = np.array(np.sqrt(locsumm.Area.air)*timeseries.loc[:,'WindSpeed'])*\
             res_time.loc[(slice(None),'air'),'Depth']*3600
--- test_tutorial.py
+++ test_tutorial.py
@@ -15,26 +15,6 @@ import matplotlib.pyplot as plt
 from BioretentionBlues import BCBlues
 import pdb
 pdb.set_trace()
-<<<<<<< Updated upstream
-#Load parameterization files
-numc = ['water', 'subsoil','rootbody', 'rootxylem', 'rootcyl','shoots', 'air','pond']
-#chemsumm = pd.read_excel('inputfiles/Kortright_ALL_CHEMSUMM.xlsx',index_col = 0)
-locsumm = pd.read_excel('inputfiles/Pine8th/Pine8th_BC.xlsx',index_col = 0)
-chemsumm = pd.read_excel('inputfiles/Pine8th/EngDesign_CHEMSUMM2.xlsx',index_col = 0)
-params = pd.read_excel('inputfiles/Pine8th/params_Pine8th.xlsx',index_col = 0)
-timeseries = pd.read_excel('inputfiles/timeseries_test.xlsx')
-Cin = 1000 #ng/L
-for compound in chemsumm.index:
-    minname = compound+'_Min'
-    timeseries.loc[:,minname] = timeseries.Qin*Cin*1/60 
-#The tracertest_Kortright_extended file was used in Rodgers et al. (2021), _test is a smaller toy dataset for 
-#testing the model
-#timeseries = pd.read_excel('inputfiles/timeseries_tracertest_Kortright_extended.xlsx')
-#Instantiate the model. In this case we will ball the model object "bioretention_cell"
-bc = BCBlues(locsumm,chemsumm,params,timeseries,numc)
-results = bc.run_BC(locsumm,chemsumm,timeseries,numc,params,pp=None)
-results.head()
-=======
 #Import the res file, load the others
 numc = ['water', 'subsoil','rootbody', 'rootxylem', 'rootcyl','shoots', 'air','pond']
 #locsumm = pd.read_excel('inputfiles/QuebecSt_TreeTrench.xlsx',index_col = 0)
@@ -73,6 +53,5 @@ mass_flux = bc.mass_flux(res,numc)
 Couts = bc.conc_out(numc,timeseries,chemsumm,res)
 Couts.loc[:,'6PPDQ_Coutest'] = Couts.loc[:,'6PPDQ_Coutest'] *1e6 #ng/L
 Couts.head()
-measdat = pd.read_excel('/inputfiles/Pine8th/Tracer_test_measurements.xlsx')
+measdat = pd.read_excel('D:/Github/SubsurfaceSinks/inputfiles\Pine8th/Tracer_test_measurements.xlsx')
 Couts.head()
->>>>>>> Stashed changes
